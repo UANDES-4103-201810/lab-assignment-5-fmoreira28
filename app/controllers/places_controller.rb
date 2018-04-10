bcaca799e3c39040ca9f85981183e0e09e4f9b1f
fmoreira28 <fjmoreira@miuandes.cl>
2018-04-10 14:04:19 -0300

    def and routs

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,37 @@
 class PlacesController < ApplicationController
+  def show
+    @place=place.find(params[:id])
+  end
+
+  def new
+    @place=place.new
+    @places=place.find(:all)
+  end
+
+  def create
+    @place=place.new([:name][:last_name][:email][:password][:address])
+    if @place.save
+      redirect_to @place
+    end
+  end
+
+  def edit
+    @place=place.find(params[:id])
+  end
+
+  def update
+    @place= place.find(params[:id])
+    if @place.update_attributes(params[:place])
+      redirect_to :action=> 'show', :id=>@place
+    else
+      @place=place.find(:all)
+      render :action=>'edit'
+    end
+  end
+
+  def destroy
+    @place=place.find(params[:id])
+    @place.destroy
+    redirect_to '/places/new', :notice=>"Your place has been Deleted"
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+  def show
+    @user=User.find(params[:id])
+  end
+
+  def new
+    @user=User.new
+    @users=User.find(:all)
+  end
+
+  def create
+    @user=User.new([:name][:last_name][:email][:password][:address])
+    if @user.save
+      redirect_to @user
+    end
+  end
+
+  def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    @user= User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to :action=> 'show', :id=>@user
+    else
+      @user=user.find(:all)
+      render json: @user
+    end
+  end
+
+  def destroy
+    @user=User.find(params[:id])
+    @user.destroy
+    redirect_to '/users/new', :notice=>"Your user has been Deleted"
+  end
 end
